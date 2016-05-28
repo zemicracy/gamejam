638349cff93508579ae86fb353cbe0219c76948b
ydakoukn <ydakoukun0105@gmail.com>
2016-05-28 17:29:57 +0900

    Main作成

---
+++ gamejam/Assets/Main/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    TimeManager m_timer;
+    [SerializeField]
+    Telop m_telop;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

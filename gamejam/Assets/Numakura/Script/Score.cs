8e25cc8b0d87b2ec7e7452a3167d088adb99f128
shana <shana@GM2140136.da.iwasaki.ac.jp>
2016-05-28 19:22:48 +0900

    すこあ～～

---
+++ gamejam/Assets/Numakura/Script/Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    private int score = 0;
+    private float timeleft;
+
+	// Use this for initialization
+	void Start () {
+        scoreText.text = "Score:0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeleft -= Time.deltaTime;
+        if (timeleft <= -2.0)
+        {
+            timeleft = 0.0f;
+            score += 10;
+            scoreText.text = "Score:" + score.ToString();
+        }
+	}
+}

9f07aee570c348d7ae7fd5ded7c4bd6684e7573f
ydakoukn <ydakoukun0105@gmail.com>
2016-05-29 14:58:33 +0900

    Item自動生成作成

---
+++ gamejam/Assets/Hashimoto/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+
+    [SerializeField]
+    int ScorePoint;
+
+    [SerializeField]
+    Sprite Object;
+
+    [SerializeField]
+    Score score;
+    void OnTriggerEnter2D(Collider2D colision)
+    {
+        Debug.Log("ゲット");
+        Destroy(this.gameObject);
+    }
+}
